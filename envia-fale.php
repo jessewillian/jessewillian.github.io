abf4944e898d9165c4f01d9bce6cab212acec732
Jesse Willian <jessewilliansousa@gmail.com>
2022-11-14 15:18:36 -0300

    test submit form

---
+++ envia-fale.php
@@ -0,0 +1,20 @@
+<?
+$destinatario = "jessewilliansousa@gmail.com";
+
+$nome = $_REQUEST['nome'];
+$email = $_REQUEST['email'];
+$mensagem = $_REQUEST['mensagem'];
+
+$body = "=====================================". "\n";
+$body = $body . "Envie um email" . "\n";
+$body = $body . "=====================================" . "\n\n";
+$body = $body . "Nome: " . $name . "\n";
+$body = $body . "Email: " . $email . "\n";
+$body = $body . "Mensagem: " . $message . "\n\n";
+$body = $body . "=====================================" . "\n";
+
+mail($destinatario ,$body, "From: $email\r\n");
+
+header("location:obrigado.htm");
+
+?>
